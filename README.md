2ea5ee709cf133135351a956418b735540f2b827
Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
2026-02-16 22:55:43 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mean/Variance/Standard Deviation Calculator
+
+This is a simple python project that gives us a facility to calculate some statistic indicators.
---
+++ mean_var_std.py
@@ -0,0 +1,12 @@
+"""
+ ===============================================================
+ mean_var_std.py
+ ===============================================================
+
+ This module file is meant to help us compute some statistical
+ indicators.
+
+ Author : Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
+ Date : 2026-02-16
+ Version : 0.0.0
+"""
