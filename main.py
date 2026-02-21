952d47108fc854704ff0532a6fbf59b4244eef02
Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
2026-02-21 06:35:35 +0100

    feat(main): implemented app entrypoint for testing.

--- main.py
+++ main.py
@@ -8,5 +8,14 @@
 
  Author : Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
  Date : 2026-02-16
- Version : 0.0.0
+ Version : 1.0.0
 """
+
+from unittest import main
+import mean_var_std
+
+# Testing the function here
+print(mean_var_std.calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
+
+# Running unit tests automatically
+main(module="test_module", exit=False)
